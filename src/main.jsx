0070b1d42049fe1ab5bd6608454457cf4fe45bf0
Naman Bhatia <namanbhatia2021@gmail.com>
2025-05-04 21:20:39 +0530

    CSS fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
       <App />
-=======
-    <App />
->>>>>>> d0c0c41d62985c15e1063ffa8c98a7159757bf69
   </React.StrictMode>,
 )
